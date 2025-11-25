de54ae6d9348ba952eb31b876cb77af3d5ef95f9
Kazan33333 <kazan.kk@gmail.com>
2025-11-25 21:01:29 +0100

    minor changes

--- js/seasonMedals.js
+++ js/seasonMedals.js
@@ -1,10 +1,10 @@
 export const seasonMedals = {
-    "2018/19": "images/pedestal-podium-svgrepo-com.svg",
-    "2019/20": "images/pedestal-podium-svgrepo-com.svg",
-    "2020/21": "images/sixth-place.svg",
-    "2021/22": "images/fourth-place.svg",
-    "2022/23": "images/silver-medal-svgrepo-com.svg",
-    "2023/24": "images/fourth-place.svg",
-    "2024/25": "images/bronze-medal-svgrepo-com.svg",
-    "2025/26": "images/pedestal-podium-svgrepo-com.svg",
+    "2018/19": "images/pedestal-podium.svg",
+    "2019/20": "images/silver-medal.svg",
+    "2020/21": "images/pedestal-podium.svg",
+    "2021/22": "images/pedestal-podium.svg",
+    "2022/23": "images/silver-medal.svg",
+    "2023/24": "images/pedestal-podium.svg",
+    "2024/25": "images/bronze-medal.svg",
+    "2025/26": "images/pedestal-podium.svg",
 };
