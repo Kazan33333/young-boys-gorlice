fbb3674ca290427393ddc9466ebbf4484c4b4e98
Kazan33333 <kazan.kk@gmail.com>
2025-12-07 17:21:01 +0100

    Update i18n.js

--- js/i18n.js
+++ js/i18n.js
@@ -48,7 +48,10 @@ const translations = {
     }
 };
 
-let currentLang = localStorage.getItem("lang") || "en";
+let storedLang = localStorage.getItem("lang");
+let browserLang = navigator.language.slice(0, 2);
+let currentLang = storedLang || (browserLang === "pl" ? "pl" : "en");
+
 
 const languagePickEnElement = document.querySelector(".language-pick-en");
 const languagePickPlElement = document.querySelector(".language-pick-pl");
