5d0621ebb0ca89f83f9ae33674b215fb505053b1
Kazan33333 <kazan.kk@gmail.com>
2025-12-16 18:55:04 +0100

    Update goatcounter.js

--- js/goatcounter.js
+++ js/goatcounter.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     const pageViewsElem = document.getElementById("pageViews");
-    if (!pageViewsElem || !window.gc) return;
+    if (!pageViewsElem) return;
 
-    gc.count().then(data => {
-        pageViewsElem.textContent = data.count;
-    }).catch(err => {
-        console.error("Błąd licznika GoatCounter:", err);
-    });
+    const path = encodeURIComponent(location.pathname);
+
+    const url = `https://kazan33333.goatcounter.com/counter/${path}.json`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            pageViewsElem.textContent = data.count;
+        })
+        .catch(err => console.error("Błąd licznika GoatCounter:", err));
 });
