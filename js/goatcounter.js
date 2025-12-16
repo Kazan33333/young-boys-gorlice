60cfa3bcbbd648921c11d9a0c30265c7ed3a244c
Kazan33333 <kazan.kk@gmail.com>
2025-12-16 18:18:12 +0100

    counting visitors

---
+++ js/goatcounter.js
@@ -0,0 +1,7 @@
+(function() {
+    var s = document.createElement("script");
+    s.setAttribute("data-goatcounter", "https://twojanazwa.goatcounter.com/count");
+    s.async = true;
+    s.src = "//gc.zgo.at/count.js";
+    document.head.appendChild(s);
+})();
